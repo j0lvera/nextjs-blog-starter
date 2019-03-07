55412bb784f72ff7d097f5265108e9e9d237c11f
thinkxl <jolvera@posteo.de>
2019-03-06 19:39:41 -0600

    fix bug with syntax highlighting

--- components/layouts/default.js
+++ components/layouts/default.js
@@ -84,6 +84,16 @@ function Layout({ path, children, pageTitle = "Next.js Blog Starter" }) {
           margin-left: auto;
           margin-right: auto;
         }
+
+        .mdx-marker {
+          background-color: rgba(255, 255, 255, 0.5);
+          display: block;
+          margin-right: -1em;
+          margin-left: -1em;
+          padding-right: 1em;
+          padding-left: 0.75em;
+          border-left: 0.25em solid #dd4a68;
+        }
       `}</style>
     </>
   );
--- components/syntax-highlight.js
+++ components/syntax-highlight.js
@@ -5,19 +5,7 @@ import theme from "prismjs/themes/prism.css";
 export default () => (
   <>
     <style jsx global>
-      {`
-        ${theme}
-
-        .mdx-marker {
-          background-color: rgba(255, 255, 255, 0.5);
-          display: block;
-          margin-right: -1em;
-          margin-left: -1em;
-          padding-right: 1em;
-          padding-left: 0.75em;
-          border-left: 0.25em solid #dd4a68;
-        }
-      `}
+      {theme}
     </style>
   </>
 );
